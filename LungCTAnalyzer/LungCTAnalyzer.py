cc06e4923439d4794cea637c748df2d1a9ce2c9c
Rudolf Bumm <rudolf.bumm@googlemail.com>
2020-12-11 19:07:38 +0100

    Removed HEAD passages from source

--- LungCTAnalyzer/LungCTAnalyzer.py
+++ LungCTAnalyzer/LungCTAnalyzer.py
@@ -184,7 +184,6 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         self.ui.toggleMaskedVolumeDisplay3DPushButton.connect('clicked()', self.onMaskedVolumeDisplay3D)
 
 
-        # Load path to PDF report directory from ini file 
         self.reportFolder = ""
 
         import configparser
@@ -200,8 +199,7 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             parser.set('reportFolder', 'path', self.reportFolder)
             with open('LCTA.INI', 'w') as configfile:    # save
                 parser.write(configfile)
-        
-        #set the widget directory path 
+
         self.ui.selectReportDirectoryButton.directory = self.reportFolder
 
 
@@ -250,6 +248,11 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             if segmentNameFragment.upper() in segmentation.GetNthSegment(segmentIndex).GetName().upper():
                 return segmentation.GetNthSegmentID(segmentIndex)
         return None
+
+    def setReportFolder(self,path):
+        self.ui.selectReportDirectoryButton.directory = path
+        self._parameterNode.SetParameter("reportFolder", path)
+
 
     def initializeParameterNode(self):
         """
@@ -553,20 +556,10 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         printer = qt.QPrinter(qt.QPrinter.PrinterResolution)
         printer.setOutputFormat(qt.QPrinter.PdfFormat)
         printer.setPaperSize(qt.QPrinter.A4)
-<<<<<<< HEAD
 
         from time import gmtime, strftime
         timestampString = strftime("%Y%m%d_%H%M%S", gmtime())
         reportPath = f"{self.reportFolder}/LungCT-Report-{timestampString}.pdf"
-=======
-        reportFolder = f"{slicer.app.defaultScenePath}/LungCTAnalyzerReports/"
-        from pathlib import Path
-        Path(reportFolder).mkdir(parents=True, exist_ok=True)
-
-        from time import gmtime, strftime
-        timestampString = strftime("%Y%m%d_%H%M%S", gmtime())
-        reportPath = f"{reportFolder}/LungCT-Report-{timestampString}.pdf"
->>>>>>> 743ea879754e9e61435f0c1fc5d112268c8c4c18
         printer.setOutputFileName(reportPath)
 
         familyName = self.logic.resultsTable.GetAttribute("LungCTAnalyzer.patientFamilyName")
@@ -646,35 +639,22 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
         axialLightboxImageFilename = "_tmp_axial_lightbox.png"
         coronalLightboxImageFilename = "_tmp_coronal_lightbox.png"
-<<<<<<< HEAD
         self.createLightboxImage("Red", self.reportFolder, axialLightboxImageFilename, reverseOrder=True)
         self.createLightboxImage("Green", self.reportFolder, coronalLightboxImageFilename)
-=======
-        self.createLightboxImage("Red", reportFolder, axialLightboxImageFilename, reverseOrder=True)
-        self.createLightboxImage("Green", reportFolder, coronalLightboxImageFilename)
->>>>>>> 743ea879754e9e61435f0c1fc5d112268c8c4c18
 
         _html+="""
         <div id="page2" class="page" style="height: 775px; width: 595px; page-break-before: always;"/>
         <h2>Axial</h2>
         <br>
         """
-<<<<<<< HEAD
         _html += f'<img src="{self.reportFolder}/{axialLightboxImageFilename}" width="500" />'
-=======
-        _html += f'<img src="{reportFolder}/{axialLightboxImageFilename}" width="500" />'
->>>>>>> 743ea879754e9e61435f0c1fc5d112268c8c4c18
 
         _html+="""
         <div id="page3" class="page" style="height: 775px; width: 595px; page-break-before: always;"/>
         <h2>Coronal</h2>
         <br>
         """
-<<<<<<< HEAD
         _html += f'<img src="{self.reportFolder}/{coronalLightboxImageFilename}" width="500" />'
-=======
-        _html += f'<img src="{reportFolder}/{coronalLightboxImageFilename}" width="500" />'
->>>>>>> 743ea879754e9e61435f0c1fc5d112268c8c4c18
 
         _html+="""
         <div id="page4" class="page" style="height: 775px; width: 595px; page-break-before: always;"/>
@@ -696,16 +676,8 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         doc.setPageSize(qt.QSizeF(printer.pageRect().size()))  # hide the page number
         doc.print(printer)
 
-<<<<<<< HEAD
         os.remove(f"{self.reportFolder}/{axialLightboxImageFilename}")
         os.remove(f"{self.reportFolder}/{coronalLightboxImageFilename}")
-=======
-        os.remove(f"{reportFolder}/{axialLightboxImageFilename}")
-        os.remove(f"{reportFolder}/{coronalLightboxImageFilename}")
-
-        # Open in PDF viewer
-        os.startfile(reportPath)
->>>>>>> 743ea879754e9e61435f0c1fc5d112268c8c4c18
 
         # Open in PDF viewer
         print("Starting '"+reportPath+"' ...")
@@ -747,9 +719,9 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         subprocess.Popen(f'explorer {os.path.realpath(self.reportFolder)}')
 
     def onReportDirectoryChanged(self):
-        # store new path locally
+        # print("Directory changed")
         self.reportFolder = self.ui.selectReportDirectoryButton.directory
-        # save new path in own ini file so that it is set each time slicer starts
+        # save new path locally
         import configparser
         parser = configparser.SafeConfigParser()
         parser.read('LCTA.INI')
