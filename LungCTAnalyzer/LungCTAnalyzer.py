ae7d11e06792a48cc883d17a53b1010fcc066c07
Rudolf Bumm <rudolf.bumm@googlemail.com>
2022-07-30 15:30:29 +0200

    MAINT: update versionhistory.txt and correct version to 2.50 in LungCTAnalyzer.py

--- LungCTAnalyzer/LungCTAnalyzer.py
+++ LungCTAnalyzer/LungCTAnalyzer.py
@@ -88,7 +88,7 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         """
         Called when the user opens the module the first time and the widget is initialized.
         """
-        self.version = 2.49
+        self.version = 2.50
         ScriptedLoadableModuleWidget.__init__(self, parent)
         VTKObservationMixin.__init__(self)  # needed for parameter node observation
         self.logic = None
