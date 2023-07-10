c1999c3cafb7746f5feb808b5599bd38879e568c
Rudolf Bumm <rudolf.bumm@googlemail.com>
2023-07-10 17:14:10 +0200

    Version change 2.69

--- LungCTAnalyzer/LungCTAnalyzer.py
+++ LungCTAnalyzer/LungCTAnalyzer.py
@@ -91,7 +91,7 @@ class LungCTAnalyzerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         """
         Called when the user opens the module the first time and the widget is initialized.
         """
-        self.version = 2.68
+        self.version = 2.69
         ScriptedLoadableModuleWidget.__init__(self, parent)
         VTKObservationMixin.__init__(self)  # needed for parameter node observation
         self.logic = None
